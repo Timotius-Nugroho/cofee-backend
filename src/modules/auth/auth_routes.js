244149429933877bfd8ffd1c55d884c9adeb0849
Timotius-Nugroho <timotiusnugroho999@gmail.com>
2021-06-18 20:05:25 +0700

    add auth API & auth middleware

--- src/modules/auth/auth_routes.js
+++ src/modules/auth/auth_routes.js
@@ -3,5 +3,9 @@ const Route = express.Router()
 const authController = require('./auth_controller')
 
 Route.post('/login', authController.login)
+Route.post('/register', authController.register)
+
+Route.get('/change-data/:token', authController.changeData)
+Route.post('/request-change-password', authController.requestChangePassword)
 
 module.exports = Route
