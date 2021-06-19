c519f6633db2d4af7dbd8c7966580ef656fb0d09
Timotius-Nugroho <timotiusnugroho999@gmail.com>
2021-06-19 12:51:58 +0700

    add auth middleware on product

--- src/modules/product/product_routes.js
+++ src/modules/product/product_routes.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const Route = express.Router()
 const uploads = require('../../middleware/uploads')
+const authMiddleware = require('../../middleware/auth')
 
 const {
   createProduct,
@@ -10,10 +11,27 @@ const {
   deleteProduct
 } = require('./product_controller')
 
-Route.post('/', uploads, createProduct)
-Route.get('/', getAllProduct)
-Route.get('/:id', getProductDataById)
-Route.patch('/:id', uploads, updateProductData)
-Route.delete('/:id', deleteProduct)
+Route.post(
+  '/',
+  authMiddleware.authentication,
+  authMiddleware.isAdmin,
+  uploads,
+  createProduct
+)
+Route.get('/', authMiddleware.authentication, getAllProduct)
+Route.get('/:id', authMiddleware.authentication, getProductDataById)
+Route.patch(
+  '/:id',
+  authMiddleware.authentication,
+  authMiddleware.isAdmin,
+  uploads,
+  updateProductData
+)
+Route.delete(
+  '/:id',
+  authMiddleware.authentication,
+  authMiddleware.isAdmin,
+  deleteProduct
+)
 
 module.exports = Route
