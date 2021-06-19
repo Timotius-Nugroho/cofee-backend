97fab45c12fd73c808a1e550a8ef9823c67a2f46
Timotius-Nugroho <timotiusnugroho999@gmail.com>
2021-06-19 21:30:56 +0700

    some product ajustment & add promo endpoint

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,10 +3,11 @@ const Route = express.Router()
 const authRouter = require('../modules/auth/auth_routes')
 const invoiceRouter = require('../modules/invoice/invoice_routes')
 const productRouter = require('../modules/product/product_routes')
+const promoRouter = require('../modules/promo/promo_routes')
 
 Route.use('/auth', authRouter)
 Route.use('/invoice', invoiceRouter)
-
 Route.use('/product', productRouter)
+Route.use('/promo', promoRouter)
 
 module.exports = Route
