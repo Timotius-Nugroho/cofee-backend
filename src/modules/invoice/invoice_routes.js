4a0d72c27d5189159fd5035bf5a13f863f5ed995
Timotius-Nugroho <timotiusnugroho999@gmail.com>
2021-06-19 11:20:54 +0700

    add auth middleware

--- src/modules/invoice/invoice_routes.js
+++ src/modules/invoice/invoice_routes.js
@@ -1,10 +1,23 @@
 const express = require('express')
 const Route = express.Router()
 const invoiceController = require('./invoice_controller')
-// const authMiddleware = require('../../middleware/auth')
+const authMiddleware = require('../../middleware/auth')
 
-Route.get('/pending', invoiceController.getInvoicePending)
-Route.post('/create', invoiceController.createInvoice)
-Route.patch('/update-status/:id', invoiceController.UpdateInvoiceStatus)
+Route.get(
+  '/pending',
+  authMiddleware.authentication,
+  invoiceController.getInvoicePending
+)
+Route.post(
+  '/create',
+  authMiddleware.authentication,
+  invoiceController.createInvoice
+)
+Route.patch(
+  '/update-status/:id',
+  authMiddleware.authentication,
+  authMiddleware.isAdmin,
+  invoiceController.UpdateInvoiceStatus
+)
 
 module.exports = Route
