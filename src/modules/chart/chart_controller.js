7bbd0abc72eca9db6c2e7c4641abe1831b008d0d
Timotius-Nugroho <timotiusnugroho999@gmail.com>
2021-06-24 22:23:44 +0700

    ajust chart API

--- src/modules/chart/chart_controller.js
+++ src/modules/chart/chart_controller.js
@@ -4,7 +4,16 @@ const chartModel = require('./chart_model')
 module.exports = {
   chartDaily: async (req, res) => {
     try {
-      const result = await chartModel.getInvoiceByDay()
+      let result = []
+      const { groubBy } = req.query
+      if (groubBy === 'month') {
+        result = await chartModel.getInvoiceByMonth()
+      } else if (groubBy === 'week') {
+        result = await chartModel.getInvoiceByWeek()
+      } else {
+        result = await chartModel.getInvoiceByDay()
+      }
+
       return helper.response(res, 200, 'Success Get Income Data By Day', result)
     } catch (error) {
       return helper.response(res, 400, 'Bad Request', error)
--- src/modules/chart/chart_model.js
+++ src/modules/chart/chart_model.js
@@ -10,5 +10,27 @@ module.exports = {
         }
       )
     })
+  },
+
+  getInvoiceByWeek: () => {
+    return new Promise((resolve, reject) => {
+      connection.query(
+        'SELECT DAYNAME(invoice_created_at) AS day_name, SUM(invoice_subtotal) AS total_invoice FROM invoice WHERE  WEEK(invoice_created_at) = WEEK(NOW()) GROUP BY DAYNAME(invoice_created_at)',
+        (error, result) => {
+          !error ? resolve(result) : reject(new Error(error))
+        }
+      )
+    })
+  },
+
+  getInvoiceByMonth: () => {
+    return new Promise((resolve, reject) => {
+      connection.query(
+        'SELECT MONTH(invoice_created_at) AS month, SUM(invoice_subtotal) AS Total FROM invoice GROUP BY MONTH(invoice_created_at)',
+        (error, result) => {
+          !error ? resolve(result) : reject(new Error(error))
+        }
+      )
+    })
   }
 }
